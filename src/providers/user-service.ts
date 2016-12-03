15ad6712d09ab551d0376da37c5e30f7ce1b3678
NadiaPC <bsbnadiapc@hotmail.com>
2016-12-03 13:34:30 -0500

    probando consumir api

--- src/providers/user-service.ts
+++ src/providers/user-service.ts
@@ -24,7 +24,7 @@ export class UserService {
 
     // don't have the data yet
     return new Promise(resolve => {
-      this.http.get('http://192.168.1.6:9000')
+      this.http.get('http://randomuser.me/api/?results=25')
         .map(res => res.json())
         .subscribe(data => {
           this.data = data.results;
